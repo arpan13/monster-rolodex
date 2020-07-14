51492b89c49e2b835f3c28baf8475435f2b82529
Arpan Kumar Pal <58396070+arpan13@users.noreply.github.com>
2020-07-15 00:06:35 +0530

    Updates

---
+++ precache-manifest.da44953b4e01965aa56b91a60ad25adc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f0fa4efd7d2570b1e0ca4ef3d2db110",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "062e895778f0a731b541",
+    "url": "/monster-rolodex/static/css/main.251584a7.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "062e895778f0a731b541",
+    "url": "/monster-rolodex/static/js/main.68d62c2d.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
